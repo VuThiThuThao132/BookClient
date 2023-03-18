97093639959af73c1bdd1ac4756719531b6376d3
Thu Thao <vuthithuthao132@gmail.com>
2023-03-18 11:01:38 +0700

    Hard adđ server host

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,7 +1,4 @@
-export const apiUrl =
-	process.env.NODE_ENV !== 'production'
-		? 'http://localhost:5000/api'
-		: 'https://booknotes.onrender.com/api'
+export const apiUrl = 'https://booknotes.onrender.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
